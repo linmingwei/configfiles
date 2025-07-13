bacebe6d7d8679d9e2e4825775119479254b3a8f
eric <linmingwei100@gmail.com>
2025-07-13 23:11:34 +0800

    update

--- nvim/lua/plugins/override.lua
+++ nvim/lua/plugins/override.lua
@@ -1,6 +1,6 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
--- if true then return {} end
+if true then return {} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
@@ -16,7 +16,7 @@ return {
       opts.keymap.preset = "none"
 
       -- 2. 手动将 <Tab> 键映射为“选择并接受”
-      opts.keymap["<Tab>"] = { "select_and_accept" }      
+      opts.keymap["<Tab>"] = { "select_and_accept" }
       -- 返回修改后的 opts
       return opts
     end,
